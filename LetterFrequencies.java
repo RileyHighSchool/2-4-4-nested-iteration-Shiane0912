ead53434e1fdfcd0e7fd851dc5fe3b2d8cf57ef4
Shiane0912 <shiane88015@students.sbcsc.k12.in.us>
2023-01-13 09:06:49 -0500

    did the assignment

--- LetterFrequencies.java
+++ LetterFrequencies.java
@@ -1,15 +1,31 @@
 /*
  * Activity 2.4.4
  */
+import java.util.Scanner;
 public class LetterFrequencies
 {
 	public static void main(String[] args)
 	{
-		String letters = "abcdefghijklmnopqrstuvwxyz"; 
-		String phrase = "This is a phrase!";
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a phrase");
+		String phrase = sc.nextLine(); 
+		phrase = phrase.toLowerCase();
+		String letters = "abcdefghijklmnopqrstuvwxyz";
 
 		System.out.println("The following shows the letter frequencies for the phrase");
 
     /* your code here */
+	for (int o = 0; o < 26; o++){
+		String let = letters.substring(o, o+1);
+		int count = 0;
+		for (int i = 0; i < phrase.length(); i++){
+			String phraseHold = phrase.substring(i,i+1);
+			if (let.equals(phraseHold)){
+				count++;
+			}
+		}
+		System.out.println(let + " appears " + count + " time(s)");
+	}
+
 	}
 }
